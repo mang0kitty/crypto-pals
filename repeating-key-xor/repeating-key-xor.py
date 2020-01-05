1b4bacce0d8aeec38c9b6fbb46c114d04a7bb41b
Aideen Fay <me@aideen.dev>
2020-01-05 19:51:36 +0000

    feat: decrypt xor

---
+++ repeating-key-xor/repeating-key-xor.py
@@ -0,0 +1,22 @@
+def repeating_key_xor(message_bytes, key):
+    output_bytes = b''
+    for i, byte in enumerate(message_bytes):
+        output_bytes += bytes([key[i % len(key)] ^ byte])
+    return output_bytes
+
+
+# def decrypt_repeating_key_xor(encrypted_message, key):
+
+
+def main():
+    key = b"ICE"
+    message = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    ciphertext = repeating_key_xor(message, key)
+    print(ciphertext.hex())
+
+    decrypted = repeating_key_xor(ciphertext, key)
+    print(decrypted.decode("utf-8"))
+
+
+if __name__ == '__main__':
+    main()
