23a102a5d0a2f1b029229fbdd1074a7906477ab7
Aideen Fay <me@aideen.dev>
2020-01-06 14:17:40 +0000

    feat: challenge 6

---
+++ breaking-repeating-key-xor/breaking-repeat-key-xor.py
@@ -0,0 +1,35 @@
+from binascii import a2b_base64
+import string
+
+
+def bytes_to_string(byte_list: bytes) -> str:
+    return "".join(filter(lambda x: x in string.printable, "".join(map(chr, byte_list))))
+
+
+def xor(b_s1, b_s2):
+    return bytes([(a ^ b) for a, b in zip(b_s1, b_s2)])
+
+
+def hamming_distance(string1: bytes, string2: bytes) -> int:
+    xor_output = xor(string1, string2)
+    return sum(bin(byte).count("1") for byte in xor_output)
+
+
+def main():
+    byte_string = b''.join([a2b_base64(line.strip())
+                            for line in open('6.txt', 'r').readlines()])
+    keysize_distances = []
+    for keysize in range(2, 40):
+        # break ciphertext into chunks the length of the keysize
+        blocks = [byte_string[i:i+keysize]
+                  for i in range(0, len(byte_string), keysize)]
+        distances = [hamming_distance(blocks[i], blocks[j])for i in range(
+            len(blocks)-1) for j in range(1, len(blocks))]
+        distance = sum(distances) / len(distances)
+        distance /= keysize
+        keysize_distances.append((keysize, distance))
+    keysize = sorted(keysize_distances, key=lambda x: x[1])[0]
+
+
+if __name__ == '__main__':
+    main()
